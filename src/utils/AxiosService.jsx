3241147b159a504585e220f7606fe24289b474fe
Vidhya0501 <vidhyasuri97@gmail.com>
2024-03-15 12:25:52 +0530

    url change

--- src/utils/AxiosService.jsx
+++ src/utils/AxiosService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let AxiosService = axios.create({
-    baseURL:"https://assign-mentorbe.onrender.com/",
+    baseURL:"http://localhost:5000",
     headers:{
         "Content-Type":"application/json"
     }
